0a4eb390c6a51edcc623047ea4c95905f4c92b0a
Marlena Tsang <Marlena345@gmail.com>
2022-01-30 22:49:16 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b ==c ||a == c
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [a,b,c].all?(&positive?)
+  end
+
+  def triangle_inequality?
+    a+b > c && b+c > a && a+c > b
+  end
+
+  def validate_triangle
+    raise TriangleError unless triangle_inequality? && sides_greater_than_zero?
+  end
+
+  class TriangleError < StandardError
+  end
 end
